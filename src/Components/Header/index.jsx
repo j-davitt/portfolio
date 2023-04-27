9f4cb899dd0b29d7e23b966882fcb095ee263974
Joe Davitt <joseph.j.davitt@gmail.com>
2023-04-27 21:21:47 +0000

    import icons

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,6 +1,7 @@
 // import { Header } from "@mantine/core";
 import { AppBar, Box, Button, Toolbar } from "@mui/material";
 import { Link } from "react-router-dom";
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const HeaderComponent = () => {
   return (
@@ -14,11 +15,12 @@ const HeaderComponent = () => {
           <Link to='/about'>
             <Button variant="contained">About Me</Button>
           </Link>
+          <Link to='https://github.com/j-davitt'>
+            <GitHubIcon />
+          </Link>
         </Toolbar>
       </AppBar>
     </Box>
-
-
 
   )
 }
