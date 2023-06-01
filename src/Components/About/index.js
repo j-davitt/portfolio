ff799f8fbae9de523df207628ac8f4ecbd6c9a5d
Joseph Davitt <joseph.j.davitt@gmail.com>
2023-05-31 18:43:48 -0700

    add job history

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -4,6 +4,12 @@ const About = () => {
   return (
     <>
       <h1>This is the About Me page!</h1>
+      <h3>Marine Corps Veteran</h3>
+      <h4>2010-2014</h4>
+      <p>Served with 3d Combat Engineer Battalion in support of OEF with 2 combat deployments.</p>
+      <h3>Foreman</h3>
+      <h4>2014-2022</h4>
+      <p>Ran excavation and landscaping crews for Four Seasons Excavating.</p>
     </>
   )
 }
