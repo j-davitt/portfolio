2d214a928c88d48eab17e6ca7ab4a2b1f1f236d8
Joseph Davitt <joseph.j.davitt@gmail.com>
2023-05-31 18:29:20 -0700

    styles

--- src/Components/Project/index.js
+++ src/Components/Project/index.js
@@ -4,29 +4,31 @@ import { projectData } from "../../assets/projectData";
 const Project = (props) => {
   return (
     <>
-      {projectData.map((project) => (
-        <Card sx={{ maxWidth: 345 }}>
-          <CardMedia
-            component="img"
-            height="140"
-            image={project.image}
-            alt={project.title}
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {project.title}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {project.description}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small" href={project.link}>Link</Button>
-            <Button size="small" href={project.github}>GitHub</Button>
-          </CardActions>
+      <div className="projects">
+        {projectData.map((project) => (
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              height="140"
+              image={project.image}
+              alt={project.title}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {project.title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {project.description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small" href={project.link}>Link</Button>
+              <Button size="small" href={project.github}>GitHub</Button>
+            </CardActions>
 
-        </Card>
-      ))}
+          </Card>
+        ))}
+      </div>
     </>
   )
 }
