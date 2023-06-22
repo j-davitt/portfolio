b90b24f693597d597cbe7daf8decc7bec4139f69
Joseph Davitt <joseph.j.davitt@gmail.com>
2023-06-22 15:36:26 -0700

    change img size

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,9 +1,19 @@
+import { Box } from '@mui/material';
+import photo from '../../assets/AboutPic.jpg';
 
 
 const About = () => {
   return (
     <>
       <h1>This is the About Me page!</h1>
+      <Box
+      sx={{
+        width: 300,
+        height: 400,
+      }}
+      >
+        <img src={require('../../assets/AboutPic.jpg')} alt="Me" />
+      </Box>
       <h3>Marine Corps Veteran</h3>
       <h4>2010-2014</h4>
       <p>Served with 3d Combat Engineer Battalion in support of OEF with 2 combat deployments.</p>
