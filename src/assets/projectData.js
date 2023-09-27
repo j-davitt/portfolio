e4dd9d487b4d72a2bc8d2eb3ef9a0f69d0894adb
Joseph Davitt <joseph.j.davitt@gmail.com>
2023-09-26 18:59:43 -0700

    add word rummage refactor project

--- src/assets/projectData.js
+++ src/assets/projectData.js
@@ -3,6 +3,14 @@
 export const projectData = [
   {
     id: 1,
+    title: 'Word Rummage Refactor',
+    description: 'Refactor of an earlier project with Mongo, Express, React, and Node.',
+    image: require('../assets/wr2.jpg'),
+    link: 'https://wordrummage2.netlify.app/',
+    github: 'https://github.com/The-Dangerzone/WordRummage-FE',
+  },
+  {
+    id: 2,
     title: 'Word Rummage',
     description: 'Word search game where the user will click through different tables and will only gain points upon selecting actual words. The game will continue to iterate until the participant runs out of time.',
     image: require('../assets/WRDemo.gif'),
@@ -10,7 +18,7 @@ export const projectData = [
     github: 'https://github.com/j-davitt/WordRummage',
   },
   {
-    id: 2,
+    id: 3,
     title: 'Pantry Pirate',
     description: 'A mobile app that allows user to create and share items for shopping lists and pantries.',
     image: require('../assets/PantryPirateLogo.png'),
@@ -18,7 +26,7 @@ export const projectData = [
     github: 'https://github.com/j-davitt/PantryPirate-FE',
   },
   {
-    id: 3,
+    id: 4,
     title: 'Terminal Trivia Trials',
     description: 'A command line interface trivia game that allows the user to select a category and difficulty level to play. Allows for multiplayer lobbies in real time.',
     image: require('../assets/TriviaDemo1.gif'),
